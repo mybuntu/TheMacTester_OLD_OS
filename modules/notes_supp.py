6ca4a6ea5ce61464fb60b234b46ea340eeeb7ce8
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-11-03 15:46:59 +0100

    Create notes_supp.py

---
+++ modules/notes_supp.py
@@ -0,0 +1,20 @@
+# The GUIless Mac Tester   
+## by Jules David
+
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+
+def add_notes(output_file):
+    # Créer la fenêtre de dialogue pour les notes
+    notes = simpledialog.askstring("Notes supplémentaires", "Ajouter des notes supplémentaires sur la machine")
+    
+    # Vérifier si des notes ont été saisies
+    if notes:
+        # Enregistrer les notes dans le fichier `resultats.txt`
+        with open(output_file, "a", encoding="utf-8") as file:
+            file.write("\n=== Notes supplémentaires : ===\n")
+            file.write(f"{notes}\n")
+        messagebox.showinfo("Notes", "Les notes supplémentaires ont été ajoutées.")
+    else:
+        messagebox.showinfo("Notes", "Aucune note n'a été ajoutée.")
+
