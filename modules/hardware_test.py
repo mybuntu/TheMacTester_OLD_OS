107d5578dd70ac0c9da4c55ef997104f1e122f98
mybuntu <101842543+mybuntu@users.noreply.github.com>
2024-11-03 15:50:31 +0100

    Update hardware_test.py

--- modules/hardware_test.py
+++ modules/hardware_test.py
@@ -1,7 +1,6 @@
 # The GUIless Mac Tester
 ## by Jules David
 import os
-import cv2
 import sounddevice as sd
 import numpy as np
 import wavio
@@ -23,26 +22,7 @@ def ask_confirmation(component):
         return "Fonctionnel"
 
 # Test des composants
-def test_camera():
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Erreur : Impossible d'ouvrir la caméra.")
-        test_results["Caméra"] = "Défectueux"
-        return
-    print("Appuyez sur 'q' pour quitter la vidéo.")
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Erreur : Impossible de lire l'image.")
-            break
-        cv2.imshow('Test de la caméra', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyWindow('Test de la caméra')
-    test_results["Caméra"] = ask_confirmation("la caméra")
 
-# Test du microphone
 def test_microphone(output_dir):
     print("Enregistrement audio...")
     audio_data = sd.rec(int(RECORD_SECONDS * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=1, dtype='float64')
@@ -76,38 +56,11 @@ def test_speakers():
     sd.wait()
     test_results["Haut-parleurs (stéréo)"] = ask_confirmation("les deux haut-parleurs")
 
-# Test Écrans (Dead-pixels)
-def test_screen():
-    print("Test de l'écran pour les pixels morts. Appuyez sur 'q' pour passer au suivant.")
-    colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (0, 255, 255)]  # Bleu, Vert, Rouge, Jaune
-    # Obtenir la résolution de l'écran avec Tkinter
-    root = tk.Tk()
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    root.withdraw()  # Masquer la fenêtre Tkinter
-    # Configurer la fenêtre OpenCV en plein écran
-    cv2.namedWindow("Test de l'écran", cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty("Test de l'écran", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    # Créer une image avec la taille complète de l’écran
-    screen = np.zeros((screen_height, screen_width, 3), dtype=np.uint8)
-
-    for color in colors:
-        screen[:, :] = color
-        cv2.imshow("Test de l'écran", screen)
-        while True:
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-    cv2.destroyWindow("Test de l'écran")
-    for _ in range(10):
-        cv2.waitKey(1)
-    test_results["Écran"] = ask_confirmation("l'écran")
-
 def run_tests(output_file, output_dir):
-    test_screen()
+    # test_screen()
     test_speakers()
     test_microphone(output_dir)
-    test_camera()
+    # test_camera()
 
     # Enregistrer les résultats dans le fichier spécifié
     with open(output_file, 'a', encoding='utf-8') as file:
